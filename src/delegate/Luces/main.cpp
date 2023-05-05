98f6c01e7506f4f571f526070ae53a7425d5506f
nicom <nicolas.mangini@ing.austral.edu.ar>
2023-05-04 22:23:49 -0300

    fix automatic lights

--- src/delegate/Luces/main.cpp
+++ src/delegate/Luces/main.cpp
@@ -1,64 +1,64 @@
 #include <Arduino.h>
 
-int LDR_Val = 0;
-int sensor = 13;
-int led = 12;
+int ldrStatus = 0;
+int ldrPin = 13;
+int ligthsPin = 12;
 
 // selectora 3 posiciones
-const int switch_pos1 = 35;
-const int switch_pos2 = 32;
+const int switchPos1 = 33;
+const int switchPos2 = 32;
 
 void setup()
 {
     Serial.begin(115200);
-    pinMode(led, OUTPUT);
+    pinMode(ligthsPin, OUTPUT);
     // switch 3 posiciones
-    pinMode(switch_pos1, INPUT_PULLUP);
-    pinMode(switch_pos2, INPUT_PULLUP);
+    pinMode(switchPos1, INPUT_PULLUP);
+    pinMode(switchPos2, INPUT_PULLUP);
 }
 
 void checkAutomaticLights()
 {
-    if (LDR_Val > 100)
+    ldrStatus = analogRead(ldrPin);
+    if (ldrStatus > 100)
     {
-        Serial.println(" High intensity: ");
-        Serial.println(LDR_Val);
-        digitalWrite(led, LOW);
+        Serial.println(" HIGH intensity: ");
+        Serial.println(ldrStatus);
+        digitalWrite(ligthsPin, LOW);
     }
     else
     {
         Serial.println("LOW Intensity ");
-        Serial.println(LDR_Val);
-        digitalWrite(led, HIGH);
+        Serial.println(ldrStatus);
+        digitalWrite(ligthsPin, HIGH);
     }
-    delay(1000);
 }
 
 void turnOnLights()
 {
     Serial.println("MANUAL ON");
-    digitalWrite(led, HIGH);
+    digitalWrite(ligthsPin, HIGH);
 }
 
 void turnOffLights()
 {
     Serial.println("MANUAL OFF");
-    digitalWrite(led, LOW);
+    digitalWrite(ligthsPin, LOW);
 }
 
 void controlLights()
 {
-    //turn on 
-    if (digitalRead(switch_pos1) == LOW)
+    // turn on
+    if (digitalRead(switchPos1) == LOW)
         turnOnLights();
-    else if (digitalRead(switch_pos1) == HIGH && digitalRead(switch_pos2) == HIGH)
+    else if (digitalRead(switchPos1) == HIGH && digitalRead(switchPos2) == HIGH)
         checkAutomaticLights();
-    else if (digitalRead(switch_pos2) == LOW)
+    else if (digitalRead(switchPos2) == LOW)
         turnOffLights();
+    delay(1000);
 }
 
 void loop()
 {
-    LDR_Val = analogRead(sensor);
     controlLights();
 }
