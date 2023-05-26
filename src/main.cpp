513d55632064f30edf6b686dc98dac0286b59bc9
nicom <nicolas.mangini@ing.austral.edu.ar>
2023-05-26 15:27:30 -0300

    forward and backward working

--- src/main.cpp
+++ src/main.cpp
@@ -1,127 +1,20 @@
-/*******************************************************************
-    A sketch for controlling a toy car with using a web page
-    hosted on a ESP8266
-
-    Main Hardware:
-    - NodeMCU Development Board cp2102 (Look for the one with the square chip beside the USB port)
-    - NodeMCU Motor Shield (L2932)
-
-    Written by Brian Lough
-    https://www.youtube.com/channel/UCezJOfu7OtqGzd5xrP3q6WA
- *******************************************************************/
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
+#include <wifi_data.h>
+#include <webpage.html>
 
-// These are the pins used to control the motor shield
-
-#define DRIVE_MOTOR_POWER 25 // Motor B
-#define DRIVE_MOTOR_DIRECTION 33
-
-#define STEER_MOTOR_POWER 26 // Motor A
-#define STEER_MOTOR_DIRECTION 32
-
-// drivePower sets how fast the car goes
-// Can be set between 0 and 1023 (although car problaly wont move if values are too low)
-int drivePower = 1023;
-
-// driveDirection sets what direction the car drives
-// If the car is moving backwards when you press the forward button, change this to LOW
-uint8_t driveDirection = HIGH;
-
-// steeringPower sets how fast the car turns
-// Can be set between 0 and 1023 (again, car probably won't steer if the value is too low)
-int steeringPower = 1023;
-
-// steerDirection sets what direction the car steers
-// If the car is steering right when you press the left button, change this to LOW
-uint8_t steerDirection = HIGH;
-
-// ----------------
-// Set your WiFi SSID and Password here
-// ----------------
-const char *ssid = "TP-Link_AC1900";
-const char *password = "01423363870";
+const int A1A = 26;
+const int A1B = 25;
+/* const int B1A = 14;
+const int B1B = 27; */
 
 WebServer server(80);
 
-const char *webpage = R"html(
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  </head>
-  <body>
-    <div class="container-fluid">
-      <div class="col-xs-12"  style="height: 100vh">
-        <div class="row" style="height: 33.33%; padding-top: 1em; padding-bottom:1em">
-          <div class="col-xs-8" ></div>
-          <div class="col-xs-4" style="text-align: center; height: 100%">
-            <button id="drive" type="button" class="btn btn-default" style="height: 100%; width: 100%" onmousedown='makeAjaxCall("forward")' onmouseup='makeAjaxCall("driveStop")' ontouchstart='makeAjaxCall("forward")' ontouchend='makeAjaxCall("driveStop")'>Drive</button>
-          </div>
-        </div>
-        <div class="row" style="height: 33.33%; padding-bottom:1em">
-          <div class="col-xs-4" style="height: 100%; text-align: center">
-            <button id="left" type="button" class="btn btn-default" style="height: 100%; width: 100%" onmousedown='makeAjaxCall("left")' onmouseup='makeAjaxCall("steerStop")' ontouchstart='makeAjaxCall("left")' ontouchend='makeAjaxCall("steerStop")'>Left</button>
-          </div>
-          <div class="col-xs-4" style="height: 100%; text-align: center">
-            <button id="right" type="button" class="btn btn-default" style="height: 100%; width: 100%" onmousedown='makeAjaxCall("right")' onmouseup='makeAjaxCall("steerStop")' ontouchstart='makeAjaxCall("right")' ontouchend='makeAjaxCall("steerStop")'>Right</button>
-          </div>
-          <div class="col-xs-4" ></div>
-        </div>
-        <div class="row" style="height: 33.33%; padding-bottom:1em">
-          <div class="col-xs-8" ></div>
-          <div class="col-xs-4" style="height: 100%; text-align: center">
-            <button id="back" type="button" class="btn btn-default" style="height: 100%; width: 100%" onmousedown='makeAjaxCall("back")' onmouseup='makeAjaxCall("driveStop")' ontouchstart='makeAjaxCall("back")' ontouchend='makeAjaxCall("driveStop")'>Back</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script> function makeAjaxCall(url){$.ajax({"url": url})}</script>
-    <!--<script>
-       document.addEventListener('keydown', function(event) {
-          if(event.keyCode == 37) {
-              //Left Arrow
-              makeAjaxCall("left");            
-          }
-          else if(event.keyCode == 39) {
-              //Right Arrow
-              makeAjaxCall("right");   
-          } else if(event.keyCode == 38) {
-              //Up Arrow
-              makeAjaxCall("forward");   
-          } else if(event.keyCode == 40) {
-              //Down Arrow
-              makeAjaxCall("back");   
-          }
-      });
-
-      document.addEventListener('keyup', function(event) {
-          if(event.keyCode == 37 ||event.keyCode == 39 ) {
-              //Left or Right Arrow
-              makeAjaxCall("steerStop");            
-          }
-          else if(event.keyCode == 38 ||event.keyCode == 40 ) {
-              //Up or Down Arrow
-              makeAjaxCall("driveStop");            
-          }
-      });
-    </script>-->
-  </body>
-</html>
-
-)html";
-
 void handleRoot()
 {
-
   server.send(200, "text/html", webpage);
 }
 
@@ -142,23 +35,15 @@ void handleNotFound()
   server.send(404, "text/plain", message);
 }
 
-void setup(void)
+void handleWifi()
 {
-
-  pinMode(DRIVE_MOTOR_POWER, OUTPUT); // Initialize the LED_BUILTIN pin as an output
-  pinMode(DRIVE_MOTOR_DIRECTION, OUTPUT);
-  pinMode(STEER_MOTOR_POWER, OUTPUT);
-  pinMode(STEER_MOTOR_DIRECTION, OUTPUT);
-
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
 
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
   Serial.println("");
 
-  // Wait for connection
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -166,7 +51,7 @@ void setup(void)
   }
   Serial.println("");
   Serial.print("Connected to ");
-  Serial.println(ssid);
+  Serial.println(SSID);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
@@ -174,51 +59,61 @@ void setup(void)
   {
     Serial.println("MDNS Responder Started");
   }
+}
+
+void setup(void)
+{
+  //Serial.begin(SERIAL_BAUD);
+  pinMode(A1A, OUTPUT);
+  pinMode(A1B, OUTPUT);
+/*   pinMode(B1A, OUTPUT);
+  pinMode(B1B, OUTPUT); */
+  handleWifi();
 
   server.on("/", handleRoot);
-
   server.on("/forward", []()
             {
     Serial.println("forward");
-    analogWrite(DRIVE_MOTOR_POWER, drivePower);
-    digitalWrite(DRIVE_MOTOR_DIRECTION, driveDirection);
+    digitalWrite(A1A, HIGH);
+    digitalWrite(A1B, LOW);
     server.send(200, "text/plain", "forward"); });
 
   server.on("/driveStop", []()
             {
     Serial.println("driveStop");
-    analogWrite(DRIVE_MOTOR_POWER, 0);
+    digitalWrite(A1A, LOW);
+    digitalWrite(A1B, LOW);
     server.send(200, "text/plain", "driveStop"); });
 
   server.on("/back", []()
             {
     Serial.println("back");
-    analogWrite(DRIVE_MOTOR_POWER, drivePower);
-    digitalWrite(DRIVE_MOTOR_DIRECTION, !driveDirection);
+    digitalWrite(A1A, LOW);
+    digitalWrite(A1B, HIGH);
     server.send(200, "text/plain", "back"); });
 
   server.on("/right", []()
             {
     Serial.println("right");
-    analogWrite(STEER_MOTOR_POWER, steeringPower);
-    digitalWrite(STEER_MOTOR_DIRECTION, steerDirection);
+/*     digitalWrite(B1A, HIGH);
+    digitalWrite(B1B, LOW); */
     server.send(200, "text/plain", "right"); });
 
   server.on("/left", []()
             {
     Serial.println("left");
-    analogWrite(STEER_MOTOR_POWER, steeringPower);
-    digitalWrite(STEER_MOTOR_DIRECTION, !steerDirection);
+/*     digitalWrite(B1A, LOW);
+    digitalWrite(B1B, HIGH); */
     server.send(200, "text/plain", "left"); });
 
   server.on("/steerStop", []()
             {
     Serial.println("steerStop");
-    analogWrite(STEER_MOTOR_POWER, 0);
+/*     digitalWrite(B1A, LOW);
+    digitalWrite(B1B, LOW); */
     server.send(200, "text/plain", "steerStop"); });
 
   server.onNotFound(handleNotFound);
-
   server.begin();
   Serial.println("HTTP Server Started");
 }
