6d8f973b4d791f9195c8096649daba376fe0bf9a
nicolas <nicolas.mangini@ing.austral.edu.ar>
2023-12-02 00:28:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Auto a control remoto con ESP32
+![BANNER](https://github.com/nicolas-mangini/esp32-rc-car-mod/assets/72108522/f1f22568-18d2-42b5-bf51-995ad033ce1d)
+
+## Elementos básicos
+- Motor: Dos motores conectados mediante un puente H para tracción y dirección
+- Luces: Dos leds con opciones de prendido/apagado manual y modo automático, haciendo uso de un LDR
+- Control: Utilizando una interfaz de joystick en una pagina web
+## Hardware (Componentes)
+- ESP32
+- Protoboard
+- Puente H L298n
+- Fuente Dc-Dc Step Down Lm2596
+- LDR
+- LED blancos
+- x2 Motores DC 6,5v
+- Batería 7,4v
+- Bornera WAGO
+- Chasis auto rc
+- Dos paragolpes con impresión 3d
+- Cables Dupont 
+- Cables UTP
+
+## Hardware (desarrollo)
+Comenzamos con la idea de construir un auto a control remoto con ESP32 utilizando un servidor para el manejo del auto.
+
+Para la base del auto se desarmó otro auto de juguete que funcionaba con radiofrecuencia.
+
+Una vez desarmado utilizamos el chasis, las ruedas y los dos motores. Uno de ellos para la dirección y otro para la tracción. Además, se aprovechó el mecanismo de giro del auto original para poder direccionar el nuevo. 
+
+Un motor se utiliza para traccionar (ir hacia adelante y hacia atrás) y otro para doblar. 
+
+Como alimentación para el auto, se discutieron múltiples opciones como pilas, pero finalmente se decidió utilizar una batería de otro auto de juguete. Debido a que la batería funciona con 7,4v y el ESP32 con 5v se necesitó añadir otro componente para manejar esta diferencia. Una opción era usar un regulador de tensión 7805 pero debido a sus mejores prestaciones terminamos optando por una fuente stepdown regulable.
+
+Para el manejo de ambos motores se utilizó un puente H L298n. Se consideraron dos modelos posibles, uno de ellos el L9110S pero terminamos optando por el L298n ya que este nos permite manejar además la velocidad de los motores. Para este manejo de la velocidad se utiliza un tercer cable por cada motor que se conecta a una entrada PWM del ESP32. La alimentación del puente H no pasa por el stepdown pues eso alimenta directamente a los motores pudiendo aprovechar los 7,4v de la batería. 
+
+Finalmente, se agregó un sistema de luces. Para esto se imprimieron en 3D dos paragolpes (delantero y trasero). En el paragolpes trasero se instaló un led que está programado para encenderse cuando se da marcha atrás y en el paragolpes delantero un led que funciona como las luces bajas de un auto con tres modos posibles: Encendido, apagado y automático. El último funciona con un LDR, prendiéndose ante la falta de luz. 
+
+## Software
+Para el manejo del auto, se levantó un servidor en el ESP32 el cual aloja una página web. La misma página provee diferentes botones para mover el auto hacia adelante, atrás, doblar a la izquierda y doblar a la derecha. 
+
+Se utiliza una slidebar para modificar solamente la velocidad del motor de tracción. En cambio, para doblar se necesita que el motor de dirección este en su máxima potencia. Cada vez que hay un cambio en el HTML se ejecuta una función donde se establece el valor de SPEED (entre 0 y 255) para que luego las demás funciones (ir hacia adelante y hacia atrás) trabajen a la velocidad seteada. 
+
+También se agregaron botones para controlar las luces, el botón de ON automáticamente pone el LED en HIGH, el off en LOW, y el AUTO ejecuta una función donde cada 1 segundo se mide la resistencia del LDR, en caso de estar debajo de un valor establecido se prende. Se utiliza la biblioteca Ticker que permite automatizar la comprobación periódica de la intensidad de luz y el control de las luces sin bloquear la ejecución principal del programa.
+
+## DEMO
+https://github.com/nicolas-mangini/esp32-rc-car-mod/assets/72108522/c88e2a87-3ad3-4a92-a44b-5ae4985a6168
+
+## Conclusión
+Mediante la combinación de componentes electrónicos y el desarrollo de software, se logró crear un sistema que permite controlar el auto a través de una interfaz web. El uso de un puente H L298N para el manejo de los motores, una fuente DC-DC Step Down para la alimentación, y un LDR para el control automático de las luces, son aspectos destacados del proyecto.
+
+El resultado final es un auto a control remoto funcional y versátil, que puede ser controlado de manera intuitiva desde cualquier dispositivo con conexión a la interfaz web. Este proyecto demuestra la integración exitosa de hardware y software en la construcción de un sistema de control remoto utilizando el ESP32 como plataforma principal.
+
+![AUTO](https://github.com/nicolas-mangini/esp32-rc-car-mod/assets/72108522/e764bb63-d8ab-4a1d-9f82-f43eed08af62)
---
+++ src/main.cpp
@@ -0,0 +1,202 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Ticker.h>
+#include <wifi_data.h>
+#include <webpage.cpp>
+
+// Motor steer
+const int ENA = 25;
+const int IN1 = 26;
+const int IN2 = 27;
+
+// Motor drive
+const int IN3 = 14;
+const int IN4 = 12;
+const int ENB = 13;
+
+int SPEED = 255;
+const int SPEED_MAX = 255;
+
+// Lights
+Ticker autoLightsTicker;
+const int LDR = 35;
+int LDR_STATUS = 0;
+const int HEADLIGHTS = 32;
+const int REVERSE_LIGHTS = 33;
+
+WebServer server(80);
+
+void handleRoot()
+{
+  //html code as char
+  server.send(200, "text/html", webpage);
+}
+
+void handleNotFound()
+{
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void handleWifi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  WiFi.begin(SSID, PASSWORD);
+  Serial.println("");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(400);
+    Serial.print(".");
+  }
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("WifiCar"))
+  {
+    Serial.println("MDNS Responder Started");
+  }
+}
+
+void checkAutomaticLights()
+{
+  LDR_STATUS = analogRead(LDR);
+  if (LDR_STATUS < 3500)
+  {
+    Serial.println("HIGH intensity: ");
+    Serial.println(LDR_STATUS);
+    digitalWrite(HEADLIGHTS, LOW);
+  }
+  else
+  {
+    Serial.println("LOW Intensity ");
+    Serial.println(LDR_STATUS);
+    digitalWrite(HEADLIGHTS, HIGH);
+  }
+}
+
+void setup(void)
+{
+  Serial.begin(SERIAL_BAUD);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(REVERSE_LIGHTS, OUTPUT);
+  pinMode(HEADLIGHTS, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(HEADLIGHTS, LOW);
+  digitalWrite(REVERSE_LIGHTS, LOW);
+  handleWifi();
+
+  server.on("/", handleRoot);
+  server.on("/forward", []()
+            {
+    Serial.println("forward");
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, SPEED);
+    server.send(200, "text/plain", "forward"); });
+
+  server.on("/driveStop", []()
+            {
+    Serial.println("driveStop");
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, 0);
+    digitalWrite(REVERSE_LIGHTS, LOW);
+    server.send(200, "text/plain", "driveStop"); });
+
+  server.on("/back", []()
+            {
+    Serial.println("back");
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENB, SPEED);
+    digitalWrite(REVERSE_LIGHTS, HIGH);
+    server.send(200, "text/plain", "back"); });
+
+  server.on("/right", []()
+            {
+    Serial.println("right");
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, SPEED_MAX);
+    server.send(200, "text/plain", "right"); });
+
+  server.on("/left", []()
+            {
+    Serial.println("left");
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH); 
+    analogWrite(ENA, SPEED_MAX);
+    server.send(200, "text/plain", "left"); });
+
+  server.on("/steerStop", []()
+            {
+    Serial.println("steerStop");
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW); 
+    analogWrite(ENA, 0);
+    server.send(200, "text/plain", "steerStop"); });
+
+  server.on("/LightsOn", []()
+            {
+    Serial.println("headlights On");
+    autoLightsTicker.detach();
+    digitalWrite(HEADLIGHTS, HIGH);
+    server.send(200, "text/plain", "Luces bajas encendidas"); });
+
+  server.on("/LightsOff", []()
+            {
+    Serial.println("headlights Off");
+    autoLightsTicker.detach();
+    digitalWrite(HEADLIGHTS, LOW);
+    server.send(200, "text/plain", "Luces bajas apagadas"); });
+
+  server.on("/LightsAuto", []()
+            {
+    Serial.println("headlights automatic");
+    autoLightsTicker.attach(1, checkAutomaticLights);
+    server.send(200, "text/plain", "Luces bajas automaticas"); });
+
+  server.on("/changeSpeed", []()
+            {
+    String speedValue = server.arg("speed");
+    Serial.println("Speed changed to " + speedValue);
+    SPEED = speedValue.toInt();
+    server.send(200, "text/plain", "Velocidad limitada a " + speedValue); });
+
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP Server Started");
+}
+
+void loop(void)
+{
+  server.handleClient();
+}
